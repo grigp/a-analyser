50753b85d0705fbb7a1ad18cd3fe479eaa4f403f
Переяслов Г. А <C:\The Bat!>
2019-04-22 18:58:00 +0300

    Вдогон

--- client_widgets/database_widgets/methodswidget.cpp
+++ client_widgets/database_widgets/methodswidget.cpp
@@ -1,6 +1,9 @@
 #include "methodswidget.h"
 #include "ui_methodswidget.h"
 
+#include "metodicdefines.h"
+#include "metodicsfactory.h"
+
 MethodsWidget::MethodsWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MethodsWidget)
