6f992108442fc53e9348e4204d1c1c65e0665ced
Переяслов(ноут) <grig_p@mail.ru>
2021-11-18 13:30:57 +0300

    Отчет по тесту асимметрии

--- metodics/stabtest/visualizators/stressstrategywidget.cpp
+++ metodics/stabtest/visualizators/stressstrategywidget.cpp
@@ -4,6 +4,17 @@
 #include "stressstrategycalculator.h"
 #include "vectorfactors.h"
 #include "dynamicdiagram.h"
+#include "reportelements.h"
+
+namespace
+{
+DynamicDiagram *wgtDiag {nullptr};
+
+QString sTarget {""};
+QString sFiveZones {""};
+QString sStressStrategyResume {""};
+
+}
 
 StressStrategyWidget::StressStrategyWidget(QWidget *parent) :
     QWidget(parent),
@@ -22,19 +33,22 @@ void StressStrategyWidget::calculate(StressStrategyCalculator *calculator, const
     Q_UNUSED(testUid);
     auto valTarget = calculator->factors(0)->factorValue(VectorFactorsDefines::KFRUid);
     auto valZones = calculator->factors(1)->factorValue(VectorFactorsDefines::KFRUid);
-    ui->lblTarget->setText(QString(tr("Мишень") + " %1 " + tr("%")).arg(QString::number(valTarget, 'f', 0)));
-    ui->lblFiveZones->setText(QString(tr("С выделенной зоной") + " %1 " + tr("%")).arg(QString::number(valZones, 'f', 0)));
+    sTarget = QString(tr("Мишень") + " %1 " + tr("%")).arg(QString::number(valTarget, 'f', 0));
+    sFiveZones = QString(tr("С выделенной зоной") + " %1 " + tr("%")).arg(QString::number(valZones, 'f', 0));
+    ui->lblTarget->setText(sTarget);
+    ui->lblFiveZones->setText(sFiveZones);
 
-    QString resume = "";
+    sStressStrategyResume = "";
     if (valTarget > valZones)
-        resume = tr("Качество выполнения пробы мишень выше, чем качество выполнения пробы с выделенной зоной");
+        sStressStrategyResume = tr("Качество выполнения пробы мишень выше, чем качество выполнения пробы с выделенной зоной");
     else
     if (valTarget < valZones)
-        resume = tr("Качество выполнения пробы с выделенной зоной выше, чем качество выполнения пробы мишень");
+        sStressStrategyResume = tr("Качество выполнения пробы с выделенной зоной выше, чем качество выполнения пробы мишень");
     else
-    if (valTarget == valZones)
-        resume = tr("Качество выполнения обеих проб одинаково");
-    ui->lblStressStrategyResume->setText(resume);
+    if (fabs(valTarget - valZones) < 2)
+        sStressStrategyResume = tr("Качество выполнения обеих проб одинаково");
+
+    ui->lblStressStrategyResume->setText(sStressStrategyResume);
 
     ui->wgtDiag->clear();
     ui->wgtDiag->setVolume(DynamicDiagram::Volume3D);
@@ -43,10 +57,70 @@ void StressStrategyWidget::calculate(StressStrategyCalculator *calculator, const
     ui->wgtDiag->appendItem(itemTarget);
     auto itemZones = new DiagItem(valZones, tr("С выделенной зоной"));
     ui->wgtDiag->appendItem(itemZones);
+
+    wgtDiag = ui->wgtDiag;
 }
 
 void StressStrategyWidget::print(QPrinter *printer, const QString &testUid)
 {
+    QPainter *painter = new QPainter(printer);
+    QRect paper = printer->pageRect();
 
+    painter->begin(printer);
+    //! Заголовок
+    QRect rectHeader(paper.x() + paper.width() / 20, paper.y() + paper.height() / 30, paper.width() / 20 * 18, paper.height() / 30 * 3);
+    ReportElements::drawHeader(painter, testUid, rectHeader);
+
+    if (printer->orientation() == QPrinter::Portrait)
+    {
+        painter->setFont(QFont("Sans", 10, QFont::Bold, false));
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 1.6),
+                          tr("Качество выполнения задания"));
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 1.9),
+                          sTarget);
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 2.2),
+                          sFiveZones);
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 2.5),
+                          sStressStrategyResume);
+
+        ReportElements::drawWidget(painter, wgtDiag,
+                                   static_cast<int>(paper.width() * 0.85), static_cast<int>(paper.height() * 0.85),
+                                   paper.x() + paper.width() / 12, paper.y() + paper.height() / 10 * 3);
+    }
+    else
+    if (printer->orientation() == QPrinter::Landscape)
+    {
+        painter->setFont(QFont("Sans", 10, QFont::Bold, false));
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 1.7),
+                          tr("Качество выполнения задания"));
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 2.0),
+                          sTarget);
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 2.3),
+                          sFiveZones);
+        painter->setFont(QFont("Sans", 14, QFont::Bold, false));
+        painter->drawText(paper.x() + paper.width() / 12,
+                          static_cast<int>(paper.y() + paper.height() / 10 * 2.8),
+                          sStressStrategyResume);
+
+        ReportElements::drawWidget(painter, wgtDiag,
+                                   static_cast<int>(paper.width() * 0.6), static_cast<int>(paper.height() * 0.6),
+                                   paper.x() + paper.width() / 10 * 2, static_cast<int>(paper.y() + paper.height() / 10 * 3.2));
+    }
+
+    //! Нижний колонтитул
+    QRect rectFooter(paper.x() + paper.width() / 20,
+                     paper.y() + paper.height() - static_cast<int>(paper.height() / 30 * 1.5),
+                     paper.width() / 20 * 18,
+                     static_cast<int>(paper.height() / 30 * 1.5));
+    ReportElements::drawFooter(painter, testUid, rectFooter);
+
+    painter->end();
 }
 
