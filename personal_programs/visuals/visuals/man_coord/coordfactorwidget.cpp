f5dfe5ed0c04315c4f50c83fe04f69060bf9356f
grig <grig_p@mail.ru>
2023-12-22 11:29:22 +0300

    Вывод всех компонентов показателей координированности в таблицы Пока нули

--- personal_programs/visuals/visuals/man_coord/coordfactorwidget.cpp
+++ personal_programs/visuals/visuals/man_coord/coordfactorwidget.cpp
@@ -7,7 +7,9 @@ CoordFactorWidget::CoordFactorWidget(QWidget *parent) :
 {
     ui->setupUi(this);
     m_mdlComponents.clear();
+    m_mdlComponents.setHorizontalHeaderLabels(QStringList() << tr("Параметр") << tr("Процент") << tr("Значение") << tr("Min") << tr("Max"));
     ui->tvComponents->setModel(&m_mdlComponents);
+//    ui->tvComponents->header()->resizeSection(0, 300);
     ui->tvComponents->setVisible(false);
 }
 
@@ -23,11 +25,30 @@ void CoordFactorWidget::setFactor(const int number, const QString &name, const d
 
 void CoordFactorWidget::setComponent(const QString &name, const QString &value, const QString &percent, const QString &min, const QString &max)
 {
+    auto itemParam = new QStandardItem(name);
+    itemParam->setEditable(false);
 
+    auto itemPercent = new QStandardItem(percent);
+    itemPercent->setEditable(false);
+
+    auto itemValue = new QStandardItem(value);
+    itemValue->setEditable(false);
+
+    auto itemMin = new QStandardItem(min);
+    itemMin->setEditable(false);
+
+    auto itemMax = new QStandardItem(max);
+    itemMax->setEditable(false);
+
+    m_mdlComponents.appendRow(QList<QStandardItem*>() << itemParam << itemPercent << itemValue << itemMin << itemMax);
+    ui->tvComponents->resizeColumnToContents(0);
 }
 
 void CoordFactorWidget::on_expand_collapse()
 {
     ui->tvComponents->setVisible(! ui->tvComponents->isVisible());
-
+    if (ui->tvComponents->isVisible())
+        ui->btnExpand->setIcon(QIcon(":/images/Collapse.png"));
+    else
+        ui->btnExpand->setIcon(QIcon(":/images/Expand.png"));
 }
